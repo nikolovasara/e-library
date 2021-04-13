d899081214b73123e8d9c0943c3280368f883091
nikolovasara <nikolovasara@hotmail.com>
2021-04-14 00:59:13 +0200

    Add CRUD methods

--- src/main/java/elibrary/wp/finki/ukim/mk/web/rest/BookRestController.java
+++ src/main/java/elibrary/wp/finki/ukim/mk/web/rest/BookRestController.java
@@ -1,17 +1,16 @@
 package elibrary.wp.finki.ukim.mk.web.rest;
 
 import elibrary.wp.finki.ukim.mk.model.Book;
+import elibrary.wp.finki.ukim.mk.model.dto.BookDto;
 import elibrary.wp.finki.ukim.mk.service.BookService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/api")
+@RequestMapping("/api/books")
 public class BookRestController {
     private final BookService bookService;
 
@@ -22,6 +21,34 @@ public class BookRestController {
     @GetMapping
     private List<Book> findAll() {
         return this.bookService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> findById(@PathVariable Long id) {
+        return this.bookService.findById(id)
+                .map(product -> ResponseEntity.ok().body(product))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Book> save(@RequestBody BookDto bookDto) {
+        return this.bookService.save(bookDto)
+                .map(product -> ResponseEntity.ok().body(product))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<Book> save(@PathVariable Long id, @RequestBody BookDto bookDto) {
+        return this.bookService.edit(id, bookDto)
+                .map(product -> ResponseEntity.ok().body(product))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteById(@PathVariable Long id) {
+        this.bookService.deleteById(id);
+        if(this.bookService.findById(id).isEmpty()) return ResponseEntity.ok().build();
+        return ResponseEntity.badRequest().build();
     }
 
 }
